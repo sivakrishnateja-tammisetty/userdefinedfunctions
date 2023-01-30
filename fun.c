c09bce2741008683a6e8fd2dec2f9292741cca59
sivakrishnateja-tammisetty <sivakrishnateja07@gmail.com>
2023-01-30 17:45:39 +0530

    updated

---
+++ fun.c
@@ -0,0 +1,378 @@
+//NO returntype & with arguments
+
+/*
+#include<stdio.h>
+void sum(int a,int b)//function definition
+{
+	int res;
+	res=a+b;
+	printf("sum is :%d\n",res);
+}
+int main()
+{
+	int a=2,b=5;
+	sum(a,b);//function calling
+	return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+void sub(int a,int b)
+{
+	int res;
+	res=a-b;
+	printf("sub is:%d\n",res);
+}
+int main()
+{
+	int a=2,b=5;
+	sub(a,b);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+void mul(int a,int b)
+{
+        int res;
+        res=a*b;
+        printf("mul  is:%d\n",res);
+}
+int main()
+{
+        int a=2,b=5;
+       mul(a,b);
+        return 0;
+}
+  */
+
+
+
+
+/*
+#include<stdio.h>
+void div(int a,int b)
+{
+        int res;
+        res=a/b;
+        printf("Div  is:%d\n",res);
+}
+int main()
+{
+        int a=2,b=5;
+       div(a,b);
+        return 0;
+}
+*/
+
+/*
+#include<stdio.h>
+void mod(int a,int b)
+{
+        int res;
+        res=a%b;
+        printf("Mod  is:%d\n",res);
+}
+int main()
+{
+        int a=2,b=5;
+       mod(a,b);
+        return 0;
+}
+*/
+//---------------------------------------------------------------------
+
+//No returntype and without arguments
+
+/*
+#include<stdio.h>
+void add()
+{
+	int a=10,b=5;
+	int c=a+b;
+	printf("Sum is %d\n",c);
+}
+int main()
+{
+	add();
+	return 0;
+}
+*/
+
+
+
+
+/*
+#include<stdio.h>
+void sub()
+{
+        int a=10,b=5;
+        int c=a-b;
+        printf("Sub  is %d\n",c);
+}
+int main()
+{
+        sub();
+        return 0;
+}
+*/
+
+
+
+
+
+/*
+#include<stdio.h>
+void mul()
+{
+        int a=10,b=5;
+        int c=a*b;
+        printf("Mul  is %d\n",c);
+}
+int main()
+{
+        mul();
+        return 0;
+}
+*/
+
+
+
+
+/*
+#include<stdio.h>
+void div()
+{
+        int a=10,b=5;
+        int c=a/b;
+        printf("Div  is %d\n",c);
+}
+int main()
+{
+       div();
+        return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+void mod()
+{
+        int a=10,b=5;
+        int c=a%b;
+        printf("Mod  is %d\n",c);
+}
+int main()
+{
+        mod();
+        return 0;
+}
+
+*/
+
+
+//-----------------------------------------------------
+
+
+
+//Returntype & without Argument
+
+/*
+#include<stdio.h>
+int add()
+{
+	int a=12,b=6;
+	int c=a+b;
+	return c;
+}
+int main()
+{
+	printf("sum is %d\n",add());
+	return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int sub()
+{
+        int a=12,b=6;
+        int c=a-b;
+        return c;
+}
+int main()
+{
+        printf("sub is %d\n",sub());
+        return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int mul()
+{
+        int a=12,b=6;
+        int c=a*b;
+        return c;
+}
+int main()
+{
+        printf("mul is %d\n",mul());
+        return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int div()
+{
+        int a=12,b=6;
+        int c=a/b;
+        return c;
+}
+int main()
+{
+        printf("Div is %d\n",div());
+        return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int mod()
+{
+        int a=12,b=6;
+        int c=a%b;
+        return c;
+}
+int main()
+{
+        printf("mod is %d\n",mod());
+        return 0;
+}
+*/
+
+//------------------------------------------------------------
+
+
+//Returntype & with Argument
+
+/*
+#include<stdio.h>
+int add(int a,int b)
+{
+	int c;
+	c=a+b;
+	return c;
+}
+int main()
+{
+	int a=27,b=3;
+	printf("sum is %d\n",add(a,b));
+	return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int sub(int a,int b)
+{
+        int c;
+        c=a-b;
+        return c;
+}
+int main()
+{
+        int a=27,b=3;
+        printf("sub is %d\n",sub(a,b));
+        return 0;
+}
+*/
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int mul(int a,int b)
+{
+        int c;
+        c=a*b;
+        return c;
+}
+int main()
+{
+        int a=27,b=3;
+        printf("mul is %d\n",mul(a,b));
+        return 0;
+}
+*/
+
+
+/*
+#include<stdio.h>
+int div(int a,int b)
+{
+        int c;
+        c=a/b;
+        return c;
+}
+int main()
+{
+        int a=27,b=3;
+        printf("Div is %d\n",div(a,b));
+        return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int mod(int a,int b)
+{
+        int c;
+        c=a%b;
+        return c;
+}
+int main()
+{
+        int a=27,b=3;
+        printf("mod is %d\n",mod(a,b));
+        return 0;
+}
+
+*/
